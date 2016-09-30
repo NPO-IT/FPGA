5afcf54ae7990c9e8e1a98c8b7c5c05e42fb2d53
OVAnechka <ocrio.box@gmail.com>
2016-09-30 10:47:01 +0300

    Формирователи орбиты всех инфйормативностей, и FIFOed UART IP core

--- globalReset.v
+++ globalReset.v
@@ -4,15 +4,19 @@
 */
 
 module globalReset			// to use this: set the initial for (~reset) and main circuit for (reset)
+#(
+	parameter clockFreq = 32'd80000000,
+	parameter delayInSec = 20
+)
 (
-	input clk40,			// 40 MHz
+	input clk,				// 40 MHz
 	output reg rst			// global enable
 );
-reg [5:0] count = 0;
+reg [63:0] count = 0;
 
-always@(posedge clk40)
+always@(posedge clk)
 begin
-	if (count > 6'b111110) rst <= 1;		// on fpga start count 62 clocks and set global enable
+	if (count > clockFreq*delayInSec) rst <= 1;		// on fpga start count 62 clocks and set global enable
 	else begin rst <= 0; count <= count + 1'b1; end		// while count set enabe to low level
 end
 endmodule
